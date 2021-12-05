c5d105d4deb97bc41502aecb611ddd8dacad68f9
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2021-12-05 08:58:11 -0600

    add template problem file to root

---
+++ problem.py
@@ -0,0 +1,16 @@
+def readInput(fileName):
+        inputFile = open(fileName, 'r')
+        return inputFile.readlines()
+
+input = readInput('input.txt')
+
+def problem1():
+    # TODO
+    return
+
+def problem2():
+    # TODO
+    return
+
+print(problem1())
+print(problem2())
