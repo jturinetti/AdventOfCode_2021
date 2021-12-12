e02e4fceb46e39f405cfa2bc1d5506394d2b92fd
Jeremy Turinetti <jeremy.turinetti@gmail.com>
2021-12-12 09:54:56 -0600

    WIP

--- 6/problem.py
+++ 6/problem.py
@@ -2,32 +2,51 @@ def readinput(fileName):
         inputfile = open(fileName, 'r')
         return list(map(lambda x : int(x), inputfile.readline().split(',')))
 
-input = readinput('input.txt')
+def lanternfish_brute_alg(filename, daycount):
+    fisharray = readinput(filename)
+    week = 7
+    currentday = week
 
-# problem 1
-fisharray = input
-daycount = 80
-week = 7
-counter = week
-
-while counter <= daycount:
-    fisharray_temp = []
+    while currentday <= daycount:
+        print(currentday)
+        fisharray_temp = []
+        for lf in fisharray:
+            if lf >= week:
+                fisharray_temp.append(lf - week)
+            else:
+                fisharray_temp.append(lf)
+                fisharray_temp.append(lf + 2)
+        fisharray = fisharray_temp
+        currentday += week
+        print(f'lanternfish count: {len(fisharray)}')
+    # handle remainder
+    remainder = daycount - (currentday - week)
+    finalcount = len(fisharray)
     for lf in fisharray:
-        if lf >= week:
-            fisharray_temp.append(lf - week)
-        else:
-            fisharray_temp.append(lf)
-            fisharray_temp.append(lf + 2)
-    fisharray = fisharray_temp
-    # print(counter)
-    # print(len(fisharray))
-    counter += week
+        if lf < remainder:
+            finalcount += 1
 
-# handle remainder
-remainder = daycount - (counter - week)
-finalcount = len(fisharray)
-for lf in fisharray:
-    if lf < remainder:
-        finalcount += 1
+    print(f'final lanternfish count: {len(fisharray)}')
+    return finalcount
 
-print(finalcount)
+def lanternfish_calculator(ttl, daysremaining):
+    return
+
+def lanternfish_alg_smart(filename, daycount):
+    fisharray = readinput(filename)
+    week = 7
+    currentday = week
+
+    # for each number, calculate total number of added lanternfish based on days remaining
+    # WIP
+    return
+
+def problem1():
+    return lanternfish_brute_alg('input.txt', 80)
+
+def problem2():
+    return lanternfish_brute_alg('test.txt', 256)
+
+
+print(problem1())
+print(problem2())
