648ab6a804b124d355fe97b865f1bec7306f01e5
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2021-12-12 21:22:02 -0600

    WIP

--- 6/problem.py
+++ 6/problem.py
@@ -8,7 +8,6 @@ def lanternfish_brute_alg(filename, daycount):
     currentday = week
 
     while currentday <= daycount:
-        print(currentday)
         fisharray_temp = []
         for lf in fisharray:
             if lf >= week:
@@ -18,34 +17,59 @@ def lanternfish_brute_alg(filename, daycount):
                 fisharray_temp.append(lf + 2)
         fisharray = fisharray_temp
         currentday += week
-        print(f'lanternfish count: {len(fisharray)}')
+        
     # handle remainder
     remainder = daycount - (currentday - week)
     finalcount = len(fisharray)
     for lf in fisharray:
         if lf < remainder:
             finalcount += 1
-
-    print(f'final lanternfish count: {len(fisharray)}')
+    
     return finalcount
 
-def lanternfish_calculator(ttl, daysremaining):
-    return
+fib_cache = [0] * 256
+fib_cache[0] = 1
+fib_cache[1] = 1
 
-def lanternfish_alg_smart(filename, daycount):
+def fib(n):
+    if (n <= 1): return 1
+    if fib_cache[n-1] == 0:
+        fib1 = fib(n-1)
+        fib_cache.insert(n-1, fib1)
+        # print(f'inserting {n-1}, {fib1}')
+    else: 
+        fib1 = fib_cache[n-1]
+
+    if fib_cache[n-2] == 0:
+        fib2 = fib(n-2)
+        fib_cache.insert(n-2, fib2)
+        # print(f'inserting {n-2}, {fib2}')
+    else:
+        fib2 = fib_cache[n-2]
+
+    return fib1 + fib2
+
+def lanternfish_calculator(ttl, daysremaining):
+    # fibonacci sequence + remainder func
+    iterations = (int)(daysremaining / 7)
+    remainder = daysremaining % 7
+    
+    return fib(iterations) + 1 if remainder > ttl else 0
+
+def lanternfish_smart_alg(filename, daysremaining):
     fisharray = readinput(filename)
-    week = 7
-    currentday = week
+    totalcount = 0
 
     # for each number, calculate total number of added lanternfish based on days remaining
-    # WIP
-    return
+    for lf in fisharray:
+        totalcount += 1 + lanternfish_calculator(lf, daysremaining)
+    return totalcount
 
 def problem1():
     return lanternfish_brute_alg('input.txt', 80)
 
 def problem2():
-    return lanternfish_brute_alg('test.txt', 256)
+    return lanternfish_smart_alg('test2.txt', 10)
 
 
 print(problem1())
