0b6d6953905831934da123081d10c651668d9442
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2022-02-06 21:17:16 -0600

    WIP

--- 8/problem.py
+++ 8/problem.py
@@ -27,6 +27,26 @@ def is_seven(signal):
 def is_eight(signal):
     return len(signal) == 7
 
+def determine_number_simple(signal):
+    if is_one(signal): return 1
+    if is_four(signal): return 4
+    if is_seven(signal): return 7
+    if is_eight(signal): return 8
+    return -1
+
+def determine_number_advanced(signal):
+    # TODO
+    return -1
+
+def determine_number(signal):
+    num = -1
+    # check 1, 4, 7, 8 first
+    num = determine_number_simple(signal)
+    if (num > -1): return num
+
+    # power of deduction!
+    return determine_number_advanced(signal)
+
 def problem1():
     inputarray = parseinput(input)
     appearancecount = 0
@@ -40,8 +60,22 @@ def problem1():
     return appearancecount
 
 def problem2():
-    # TODO
+    inputarray = parseinput(input)
+    running_total = 0
+    char_signal_mapping = [-1] * 10     # -1 indicates the signal string is not deduced yet
+    print(char_signal_mapping)
+
+    # loop over every line of input
+    for line in inputarray:
+        # loop through to find simple numbers first
+        for signal_chars in line[0]:
+            num = determine_number_simple(signal_chars)
+            if (num > -1): char_signal_mapping[num] = ''.join(sorted(signal_chars))
+        # loop again to see what we can determine
+        
+
+    print(char_signal_mapping)
     return
 
 print(problem1())
-# print(problem2())
+print(problem2())
