ed7521f1283e1cb51a1883bc61aa3d68480f3773
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2022-02-07 23:41:11 -0600

    WIP

--- 8/problem.py
+++ 8/problem.py
@@ -1,6 +1,29 @@
+#    a(0)
+# b(1)     c(2)
+#    d(3)
+# e(4)     f(5)
+#    g(6)   
+
+# find 1, 4, 7, and 8
+# c, f (which is which still unknown)
+
+# determine top signal (7 chars - 1 chars)
+# a
+
+# determine middle signal (duplicate between 3 chars and 4 chars, minus c and f)
+# d
+
+# determine bottom signal (3 chars, the one we don't know)
+# g
+
+# determine bottom left signal (2 chars - the ones we know)
+# e
+
+# determine upper left signal (8 chars - the ones we know)
+# b
 def readInput(fileName):
-        inputFile = open(fileName, 'r')
-        return inputFile.readlines()
+    inputFile = open(fileName, 'r')
+    return inputFile.readlines()
 
 
 input = readInput('input.txt')
@@ -34,19 +57,6 @@ def determine_number_simple(signal):
     if is_eight(signal): return 8
     return -1
 
-def determine_number_advanced(signal):
-    # TODO
-    return -1
-
-def determine_number(signal):
-    num = -1
-    # check 1, 4, 7, 8 first
-    num = determine_number_simple(signal)
-    if (num > -1): return num
-
-    # power of deduction!
-    return determine_number_advanced(signal)
-
 def problem1():
     inputarray = parseinput(input)
     appearancecount = 0
@@ -62,19 +72,40 @@ def problem1():
 def problem2():
     inputarray = parseinput(input)
     running_total = 0
-    char_signal_mapping = [-1] * 10     # -1 indicates the signal string is not deduced yet
-    print(char_signal_mapping)
+
+    # array of strings that map to digits (digit is the index)
+    # -1 indicates the signal string is not deduced yet
+    digit_string_mapping = [-1] * 10
+    
+    # array of chars that map to signal position (index is the position (a = 0, b = 1, etc.))
+    # -1 indicates the char is not deduced yet
+    char_pos_mapping = [-1] * 10
 
     # loop over every line of input
     for line in inputarray:
         # loop through to find simple numbers first
         for signal_chars in line[0]:
             num = determine_number_simple(signal_chars)
-            if (num > -1): char_signal_mapping[num] = ''.join(sorted(signal_chars))
-        # loop again to see what we can determine
-        
+            if (num > -1): digit_string_mapping[num] = ''.join(sorted(signal_chars))
+        # upper and lower right signals
+        char_pos_mapping[2] = digit_string_mapping[1][0]
+        char_pos_mapping[5] = digit_string_mapping[1][1]
+        # determine top signal (7 - 1)
+        # digit_string_mapping[7] = 
 
-    print(char_signal_mapping)
+
+        # FINALLY loop over 4 digits, create number, etc.
+        str_num = ''
+        for digit in line[1]:
+            i = 0
+            while i < len(digit_string_mapping):
+                if digit_string_mapping[i] == digit:
+                    str_num += str(i)
+                i += 1
+        # print(str_num)
+
+    print(char_pos_mapping)
+    print(digit_string_mapping)
     return
 
 print(problem1())
